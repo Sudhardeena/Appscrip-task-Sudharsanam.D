82fbc85aecdb3f5427769e7398b61b36150d6be1
Sudhardeena <sudharsanam16499@gmail.com>
2024-12-11 23:44:20 +0530

    Basic page layout

---
+++ src/app/components/ProductList/ProductList.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./ProductList.module.css";
+
+const ProductList = () => {
+  return <ul className={styles.productList}>ProductList</ul>;
+};
+
+export default ProductList;
---
+++ src/app/components/Toggler/Toggler.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./Toggler.module.css";
+
+const Toggler = () => {
+  return <div className={styles.togglerContainer}>Toggler</div>;
+};
+
+export default Toggler;
